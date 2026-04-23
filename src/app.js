2f84148d49c94888f185b7ca3a6a9f465b8a709a
Szász Benedek <benedekszasz7@gmail.com>
2026-04-23 13:22:48 +0200

    kezdés

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import * as bootstrap from 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './style.css'
+
+const propertyList = [
+	{id: 1, type: 'for sale', price: 65000, city: 'Pécs', baseArea: 80},
+	{id: 2, type: 'for sale', price: 102000, city: 'Pécs', baseArea: 75},
+	{id: 3, type: 'for sale', price: 89000, city: 'Pécs', baseArea: 120},
+	{id: 4, type: 'for sale', price: 48000, city: 'Szeged', baseArea: 67},
+	{id: 5, type: 'for sale', price: 57000, city: 'Hatvan', baseArea: 98},
+	{id: 6, type: 'for rent', price: 90, city: 'Pécs', baseArea: 72},
+]
