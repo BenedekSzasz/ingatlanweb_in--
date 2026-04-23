19cd144784719052c8ab2cd75e525d36f86b9a15
Szász Benedek <benedekszasz7@gmail.com>
2026-04-23 16:10:10 +0200

    Mentésgomb létrehozásának elkezdése

--- src/app.js
+++ src/app.js
@@ -13,14 +13,16 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './style.css'
 import Swal from 'sweetalert2'
 
-const propertyList = [      //lista, tömb
-	{id: 1, type: 'for sale', price: 65000, city: 'Pécs', baseArea: 80},
-	{id: 2, type: 'for sale', price: 102000, city: 'Pécs', baseArea: 75},
-	{id: 3, type: 'for sale', price: 89000, city: 'Pécs', baseArea: 120},
-	{id: 4, type: 'for sale', price: 48000, city: 'Szeged', baseArea: 67},
-	{id: 5, type: 'for sale', price: 57000, city: 'Hatvan', baseArea: 98},
-	{id: 6, type: 'for rent', price: 90, city: 'Pécs', baseArea: 72},
-]
+import { 
+    getProperties, 
+    createProperty 
+} from './propertyService.js'
+
+      //lista, tömb
+
+
+console.log(await getProperties())
+var propertyList = await getProperties()
 
 const doc = { 
     tbody: document.querySelector('#tbody'),
@@ -48,10 +50,12 @@ propertyList.forEach(prop => {
 doc.tbody.innerHTML = rows
 
 doc.aboutButton.addEventListener('click', () => {
+    
     Swal.fire({
         title: "Ingatlan",
         text: "Szász Benedek, IN, 2026-04-23",
     })
-        
-    
 })
+
+
+
---
+++ src/propertyService.js
@@ -0,0 +1,34 @@
+
+const url = "http://localhost:8000/properties"
+
+async function getProperties () {
+    try {
+        const response = await fetch(url)
+        const result = await response.json()
+        return result
+    } catch (error) {
+        console.error("Hiba! Az ingatlanok letöltése hibás")
+        console.err;
+    }
+    return "Valami";
+}
+
+async function createProperty (property){
+        try {
+        const response = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify(property),
+            headers:{
+                "Content-Type": "application/json"
+            }
+        })
+        const result = await response.json()
+        return result
+    } catch (error) {
+        console.error("Hiba! Az új ingatlan felvétele hibás")
+        console.err;
+    }
+    return "Valami";
+}
+
+export { getProperties, createProperty }
